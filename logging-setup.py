8499da91abe7efe163ac14e9582b2999976e2b77
zgwldrc <zgwldrc@163.com>
2017-10-27 04:59:42 -0500

    Create logging-setup.py

---
+++ logging-setup.py
@@ -0,0 +1,16 @@
+import logging
+
+
+_DEFAULT_LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+
+_LOGGER = logging.getLogger(__name__)
+
+def _configure_logging(loglevel=logging.INFO):
+    _LOGGER.setLevel(loglevel)
+
+    ch = logging.StreamHandler()
+
+    formatter = logging.Formatter(_DEFAULT_LOG_FORMAT)
+    ch.setFormatter(formatter)
+
+    _LOGGER.addHandler(ch)
