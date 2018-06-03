cd39840f3f7e9bf9117c3dd47cdd2cffa0d6703c
zgwldrc <zgwldrc@163.com>
2018-06-03 10:31:05 +0800

    Create reconnect.py

---
+++ reconnect.py
@@ -0,0 +1,16 @@
+retries = 0
+try:
+    r = self.session.request(method, url, *args, **kwargs)
+except requests.exceptions.ConnectTimeout:
+    retries += 1
+
+# reconnect is needed
+if retries > 0:
+    while retries < self.max_reconnect + 1:
+        logger.error('connecting to {} timeout, retry({}/{})'.format(url, retries, self.max_reconnect))
+        try:
+            r = self.session.request(method, url, *args, **kwargs)
+        except requests.exceptions.ConnectTimeout:
+            retries += 1
+        else:
+            break
